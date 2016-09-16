792d5d507e187fd987ea9fcf9ba957598788ac58
Jorgen Hookham <Jorgen@Hookham.co>
2016-09-16 11:57:03 -0700

    better live reload

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function (grunt) {
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        connect: {
+            server: {
+                options: {
+                    port: 9001,
+                    base: './'
+                }
+            }
+        },
+
+        clean: {
+            compiled: {
+                files: [{
+                    expand: true,
+                    cwd: 'compiled/',
+                    src: '**/*',
+                }]
+            }
+        },
+
+        sass: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'development/styles/',
+                    src: '**/*.scss',
+                    dest: 'compiled/styles/',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        watch: {
+            sass: {
+                options: {interrupt: true},
+                files: ['development/styles/**/*.scss'],
+                tasks: ['sass:dist']
+            },
+            styles: {
+                options: {livereload: true},
+                files: ['compiled/styles/**/*.css']
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['clean', 'sass', 'connect', 'watch']);
+
+}
