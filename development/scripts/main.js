056b29b8b8a67e2cbf3cf96b00661398dbb7a62f
Jorgen Hookham <Jorgen@Hookham.co>
2016-09-18 09:48:38 -0700

    carousel first pass

--- development/scripts/main.js
+++ development/scripts/main.js
@@ -8,3 +8,10 @@ $('.grid-item').on('mouseout', function (e) {
         $('.grid').removeClass('pause');
     }, 320);
 });
+
+$('.carousel-nav > li').on('click', function (e) {
+    for (var i = 0; i < $('.carousel-nav > li').length; i++) {
+        $('.carousel').removeClass('active-item-' + (i + 1));
+    }
+    $('.carousel').addClass('active-item-' + $(e.currentTarget).data('index'));
+});
